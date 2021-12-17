5d9aea25d12b015da333c56bcd1b76dead0cbd49
adalkolmo <antonin.dalkolmo@gmail.com>
2021-12-17 09:23:26 +0100

    feat(Data)
    Models declarations

---
+++ Stive.Client/Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stive.Client.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; } 
+        public string? Label { get; set; }   
+    }
+}
