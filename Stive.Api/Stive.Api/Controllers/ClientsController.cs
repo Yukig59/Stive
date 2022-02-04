a4ec2ec2f25f0bc6cfbea78fca853deabc49dfe5
Pierre-Alain <p.petrel@attineos.com>
2022-02-04 13:21:59 +0100

    ajout path /login dans clients controller

--- Stive.Api/Stive.Api/Controllers/ClientsController.cs
+++ Stive.Api/Stive.Api/Controllers/ClientsController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Api.Data;
 using api.Data.Models;
-using Microsoft.AspNetCore.JsonPatch;
 
 namespace Stive.Api.Controllers
 {
@@ -102,28 +101,30 @@ namespace Stive.Api.Controllers
             return NoContent();
         }
 
-
-        // PATCH : api/Inventaire/5
-        [HttpPatch("{id}")]
-        public IActionResult Patch(int id, [FromBody] JsonPatchDocument<Clients> patchEntity)
+        //route de connexion retourn
+        [Route("/login")]
+        [HttpPost]
+        public Boolean PostLoging(Clients clients)
         {
-            var entity = _context.Clients.FirstOrDefault(client => client.Id == id);
+            string email = clients.Email;
+            string password = clients.Password;
 
-            if (entity == null)
+            var result = _context.Clients.Where(b => clients.Email == email && clients.Password == password);
+
+            if (result != null)
             {
-                return NotFound();
+                return true;
             }
-
-            patchEntity.ApplyTo(entity, ModelState); // Must have Microsoft.AspNetCore.Mvc.NewtonsoftJson installed
-
-            return Ok(entity);
+            else
+            {
+                return false;
+            }
         }
+
 
         private bool ClientsExists(int id)
         {
             return _context.Clients.Any(e => e.Id == id);
         }
-
-
     }
 }
